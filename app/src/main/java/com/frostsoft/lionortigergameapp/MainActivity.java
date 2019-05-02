dfc78fa97d311dd36d9800be3fed957177d20f4d
FrostWolf <tabis.rafal@gmail.com>
2019-05-02 22:42:31 +0200

    [LoTGame] init commit

---
+++ app/src/main/java/com/frostsoft/lionortigergameapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.frostsoft.lionortigergameapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
